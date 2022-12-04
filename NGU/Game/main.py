ba0dff486d26e9dd3779e158ca652f325d7d8e00
KonstantinL037 <k.nosov@g.nsu.ru>
2022-12-04 17:14:29 +0700

    Добавил функцию player_ans, которая не дает ввести недопустимый для игры ответ.

--- NGU/Game/main.py
+++ NGU/Game/main.py
@@ -7,6 +7,28 @@ player = Class.Stats("Script.txt", 50, 50, 50, 50)
 Class.Menu.loading()
 Class.Menu.loading_text("text.txt")
 
+
+def player_ans():
+    tries = 1
+    player_answer = int(input("Выберите вариант: "))
+
+    while player_answer != 1 and player_answer != 2:
+        if tries <= 3:
+            print('Милорд мы же вас предупреждали!\nТолько 1 или 2!')
+            player_answer = int(input("Выберите вариант: "))
+            tries += 1
+        elif 4 <= tries <= 7:
+            print('Милорд с вами все в порядке? Пожалуйста выберите 1 или 2!')
+            player_answer = int(input("Выберите вариант: "))
+            tries += 1
+        elif tries > 7:
+            print('Милорд мы можем с вами спорить бесконечно, но ситуация не изменится! 1 или 2!')
+            player_answer = int(input("Выберите вариант: "))
+            tries += 1
+    else:
+        return player_answer
+
+
 while player.check_stat():
 
     player.get_stats()
@@ -18,7 +40,7 @@ while player.check_stat():
     out_menu.print_ans2()
     time.sleep(1)
     print(5 * "\n")
-    player_ans = int(input("Выберите вариант: "))
-    player.change_stat(player_ans, out_menu.ans1, out_menu.ans2)
+    ans = player_ans()
+    player.change_stat(ans, out_menu.ans1, out_menu.ans2)
 
 print("ВЫ ПРОИГРАЛИ!")
